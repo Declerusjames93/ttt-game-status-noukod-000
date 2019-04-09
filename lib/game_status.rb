e4090b8b555d2cb5042ee0b983bb96a56090912e
James Declerus <james.declerus@esih.edu>
2019-04-09 15:38:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,10 +32,11 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
+    else
+          return false
+        end
 }
-else
-      return false
-    end
+
 
 
 #definition of the full function
