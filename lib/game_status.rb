c34e362c6238382c22b9c4b314de88a3daef4ba7
James Declerus <james.declerus@esih.edu>
2019-04-09 14:28:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,3 +50,16 @@ def draw?(board)
     return true
   end
 end
+#definition of winner function
+def winner(board)
+  if !won?(board)
+    return nil
+  else WIN_COMBINATIONS.each do |win_IT|
+    if check_win_combination?(board, 'X', win_IT)
+      return 'X'
+    elsif check_win_combination?(board, 'O', win_IT)
+      return 'O'
+    end
+  end
+end
+end
